f871ac98b2c81920386bcfe09a77b32d93a8d053
Paulo Afonso Batista Gonçalves <paulo.abg@hotmail.com>
2021-07-05 16:08:29 -0300

    implementando primeira requisicao

--- src/main/java/br/com/alura/forum/modelo/Curso.java
+++ src/main/java/br/com/alura/forum/modelo/Curso.java
@@ -13,6 +13,9 @@ public class Curso {
 	private Long id;
 	private String nome;
 	private String categoria;
+	
+	public Curso() {
+	}
 
 	public Curso(String nome, String categoria) {
 		this.nome = nome;
