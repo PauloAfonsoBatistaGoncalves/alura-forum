e21e2d6f909219789575be242f2f8949c73aad34
Paulo Afonso Batista Gonçalves <paulo.abg@hotmail.com>
2021-07-05 18:59:23 -0300

    implementando bean validation

--- src/main/java/br/com/alura/forum/controller/form/TopicoForm.java
+++ src/main/java/br/com/alura/forum/controller/form/TopicoForm.java
@@ -1,8 +1,24 @@
 package br.com.alura.forum.controller.form;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+
 public class TopicoForm {
+	
+	@NotNull
+	@NotEmpty
+	@Length(min = 5)
 	private String titulo;
+	
+	@NotNull
+	@NotEmpty
+	@Length(min = 10)
 	private String mensagem;
+	
+	@NotNull
+	@NotEmpty
 	private String nomeCurso;
 	
 	public String getTitulo() {
